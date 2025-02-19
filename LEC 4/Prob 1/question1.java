1563000850b21d63dacf7dca3c91b5daf9f4508b
221B280 <221B280@juet.co.in>
2025-02-19 14:27:04 +0530

    lab 4

---
+++ LEC 4/Prob 1/question1.java
@@ -0,0 +1,11 @@
+// Make a class Mother with int x and void show ( ) as its members. Now create another
+// class Child that is derived from the Mother class. Create another class Application to
+// test the inheritance. 
+
+public class question1 {
+    public static void main(String[] args) {
+        Mother m=new Mother();
+        Child c=new Child();
+        c.show();
+    }
+}
